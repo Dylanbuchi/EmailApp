a0bf0d44c84608293116e345b236d1674d3635b4
Dylan <devybuchi@gmail.com>
2019-10-16 11:57:31 -0300

    finished project

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -8,31 +8,53 @@ public class Email {
 	private String lastName;
 	private String password;
 	private String department;
+	private String email;
 	private String alternateEmail;
-	private int mailBoxCapacity;
+	private String companyNameUrl;
+	private boolean emailValid;
 
-	// Constructor to receive the first and last name + department.
+	private int mailBoxCapacity;
+	private int defaultPasswordLength;
+
+	// Constructor to receive the first and last name
 	public Email(String firstName, String lastName) {
 
 		this.firstName = firstName;
 		this.lastName = lastName;
 
-		System.out.println("Email created with success! : " + firstName + " " + lastName);
-		
+		// initialize variables
+		this.defaultPasswordLength = 7;
+		this.companyNameUrl = ".BigDevs.com";
+		this.mailBoxCapacity = 500;
+		this.emailValid = true;
+
+		// Call a method to ask for department
 		this.department = setDepartment();
-		System.out.println("Department: " + this.department);
+
+
+		// Call a method to set a random password
+		this.password = setPassword(this.defaultPasswordLength);
+		System.out.println("Your default password is: " + this.password);
+
+		// initialize the email
+		this.email = this.firstName.toLowerCase() + "." + this.lastName.toLowerCase() + "@" + this.department.toLowerCase() + this.companyNameUrl.toLowerCase();
+
+		// print email
+		System.out.println("Your email is: " + email);
+		System.out.println("Your email was created with succes!");
+		
 
 	}
 
-	// Ask the user for the department.
+	// Ask the user for the department
 	public String setDepartment() {
 
-		System.out.print("Select your department: 1 for Sales, 2 for Development, 3 for Accounting or 0 for nothing: ");
+		System.out.print("Select your department: 0 for User, 1 for Sales, 2 for Development, or 3 for Accounting: ");
 		Scanner in = new Scanner(System.in);
 		int UserNumber = in.nextInt();
 
 		if (UserNumber == 1) {
-			return "sales";
+			return "Sales";
 
 		} else if (UserNumber == 2) {
 			return "Development";
@@ -41,8 +63,71 @@ public class Email {
 			return "Accounting";
 
 		} else {
-			return "";
+			return "user";
 		}
 
 	}
+
+	// Set a random password
+	public String setPassword(int length) {
+
+		String password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$?";
+
+		char[] randomPassword = new char[length];
+
+		for (int i = 0; i < length; i++) {
+
+			int random = (int) (Math.random() * password.length());
+			randomPassword[i] = password.charAt(random);
+
+		}
+
+		return new String(randomPassword);
+
+	}
+
+	// Method to change the password
+	public void changePassword(String password) {
+		this.password = password;
+
+	}
+
+	// Set the mailBox capacity
+	public void setMailBoxCapacity(int capacity) {
+		this.mailBoxCapacity = capacity;
+
+	}
+
+	// Set the alterEmail
+	public void setAlternateEmail(String email) {
+		this.alternateEmail = email;
+
+	}
+
+
+
+	// Get methods
+	public String getPassword() {
+		return this.password;
+	}
+
+	public String getAlternateEmail() {
+		return this.alternateEmail;
+	}
+
+	public int getMailBoxCapacity() {
+		return this.mailBoxCapacity;
+	}
+
+	// Method to show info about a new user
+
+	public String showInfo() {
+		System.out.println();
+
+		return "NAME: " + firstName + " " + lastName + "\n" + "COMPANY EMAIL: " + email + "\n" + "EMAIL BOX STORAGE: "
+				+ mailBoxCapacity + "MB" + "\n" + "DEPARTMENT: " + department + "\n" + "DEFAULT PASSWORD:  " + password
+				+ "\n" + "EMAIL VALID: " + emailValid;
+
+	}
+
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -3,7 +3,9 @@ package emailapp;
 public class EmailApp {
 
 	public static void main(String[] args) {
-		Email email1 = new Email("Dylan", "Buchi");
+		Email user1 = new Email("Dylan", "Buchi");
+		
+		System.out.println(user1.showInfo());
 		
 		
 		
